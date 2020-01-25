6853295090116e2634024a3c950c35f9f169ced1
Erica Rodrigues <erica_rodrigues.ads@hotmail.com>
2020-01-25 12:01:03 -0300

    Criando a página de favorito, mas ainda está em contrução

---
+++ src/pages/Favorite/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import LayoutMain from '../../components/LayoutMain'
+import { Row, Col } from 'antd';
+
+interface IFavoriteProps {
+  path: string
+}
+
+const Favorite: React.FC<IFavoriteProps> = () => {
+  return (
+    <LayoutMain title='Favorito'>
+      <Row type="flex" justify="center">
+        <Col span={8}>
+           <h1 style={{ textAlign:'center' }}>Encontrução a página de favorito :)</h1>     
+        </Col>
+      </Row>
+    </LayoutMain>
+  )
+}
+
+export default Favorite
+
