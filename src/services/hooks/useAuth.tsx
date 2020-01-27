adf51311d475fa6b68df099b133f939a0483bc37
Erica Rodrigues <erica_rodrigues.ads@hotmail.com>
2020-01-27 01:40:26 -0300

    Retirando o hooks do usuário e deixando mais simples

--- src/services/hooks/useAuth.tsx
+++ src/services/hooks/useAuth.tsx
@@ -1,68 +1,34 @@
-import React, { createContext, useState, useContext, useEffect } from 'react'
+import React, { createContext, useState, useContext, useCallback } from 'react'
 import { fbAuth } from '../firebase'
-import Preloader from '../../components/Preloader'
 
 export interface IAuthState {
-  initialized: boolean;
   isLoggedIn: boolean;
-  user?: firebase.UserInfo;
+  name: string | null | undefined;
 }
 
 const initialState: IAuthState = {
-  initialized: false,
   isLoggedIn: false,
-  user: undefined,
+  name: '',
 }
 
-const StateContext = createContext<IAuthState>(initialState)
-type SetAuthState = React.Dispatch<React.SetStateAction<IAuthState>>
+const useAuth = () => {
+  const [isLoggedIn , setIsLoggedIn] = useState(false);
 
-
-const AuthContainer = async (setState: SetAuthState) => {
-  console.log("oi")
- await fbAuth.onAuthStateChanged(auth => {
-   console.log("entrou")
-    setState(oldState => ({ ...oldState, isLoaded: true }));
-    if(auth) {
-      console.log('Usuário logou.');
-      const userJson = auth.toJSON() as firebase.UserInfo;
-
-      // Adicionar state com os dados do usuário
-      setState(oldState => ({ ...oldState, user: userJson, isLoggedIn: true }));
-    } else {
-      console.log('Usuário deslogou.');
-
-      // Remove informações do usuário
-      setState(oldState => ({ ...oldState,
-        isLoggedIn: false,
-        user: undefined,
-      }));
-    }
-   
-  });
-};
-
-export const Provider: React.FC = ({ children }) => {
-  const [authState, setAuth] = useState(initialState);
-
-  useEffect(() => {
-    AuthContainer(setAuth);
-  }, [])
-
-  if (!authState.initialized) {
-    return <Preloader text="Loading authentication" />;
-  }
-
-  return (
-    <StateContext.Provider value={authState}>{children}</StateContext.Provider>
+  const updateState = useCallback((value) => {
+    setIsLoggedIn(value)
+  }, [status]
   )
+
+  return [{ isLoggedIn, updateState }]
+
 }
+
 
 export const logOut = () => {
   fbAuth.signOut();
 }
 
-export default () => {
-  const state = useContext(StateContext);
-  return [state];
-}
+
+export default useAuth
+
+
