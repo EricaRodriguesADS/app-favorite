6fbdb04d21aacb856aa5131456859d6e83b8e9dd
Erica Rodrigues <erica_rodrigues.ads@hotmail.com>
2020-01-27 01:36:23 -0300

    Adicionando o menu do login

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,17 +1,33 @@
-import React, { useState } from 'react'
-import MenuItem from './pure'
+import React, { useState, useEffect } from 'react'
+import { fbAuth } from '../../services/firebase'
+import MenuItem, { ICustomMenu } from './pure'
 import { useLocation } from 'wouter'
+import { routes } from '../../store'
 
 const  ControlledMenu = () => {
+  const [user, setUser ] = useState();
   const [location, setLocation] = useLocation();
+
   const[currentKeys, setCurrentKeys] = useState(location);
+  const [customMenu, setCustomMenu] = useState<ICustomMenu>({titulo: 'Faça seu login', route: routes.auth});
+
+  useEffect(() => {
+      setUser(fbAuth.currentUser);
+  },[user,fbAuth.currentUser])
+
+  useEffect(()=>{
+    if(user) setCustomMenu({titulo: 'Seu perfil', route: routes.auth })
+    else { 
+      setCustomMenu({titulo: 'Faça seu login', route: routes.auth});
+    }
+  },[customMenu.route, user])
 
   const onClickItem = (url:string) => {
     setCurrentKeys(url);
     setLocation(url);
   }
 
-  return(<MenuItem stateKeys={currentKeys} onClickItem={onClickItem} />)
+  return(<MenuItem stateKeys={currentKeys} onClickItem={onClickItem} customMenu={customMenu} />)
 }
 
 export default ControlledMenu;
