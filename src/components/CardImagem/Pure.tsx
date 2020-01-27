d8c97d61d401ed276cc453bb3287c0494f4e50c5
Erica Rodrigues <erica_rodrigues.ads@hotmail.com>
2020-01-27 01:35:00 -0300

    Adicionando os efeitos no cards

--- src/components/CardImagem/Pure.tsx
+++ src/components/CardImagem/Pure.tsx
@@ -1,7 +1,40 @@
-import React, {FC} from 'react';
-import { Col } from 'antd';
-import { Card } from 'antd';
+import React, {FC, useState, useEffect} from 'react';
+import styled from 'styled-components'
+import { Col, Card, Icon } from 'antd';
 import 'antd/dist/antd.css';
+
+
+const Link = styled(Card)`
+  margin: 0;
+  height: 185px; 
+  width:150px;
+  white-space: pre;
+  :hover {
+    background-color: #e2e2e2e3;
+    height: 220px;
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+
+    .ant-card-meta-description{
+      display: block;
+    }
+  }
+  
+  .ant-card-meta-title{
+    color:#000;
+  }
+
+  .ant-card-meta-description{
+    display:none;
+  }
+`
+const IconCustom = styled(Icon)`
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  border: none;
+  cursor: pointer;
+`
+
 
 interface ICardImagemProps {
   src: string;
@@ -19,14 +52,29 @@ const CardImagem:FC<ICardImagemProps> = ({
   colSize,
 }) => {
   const { Meta } = Card;
-  return(<Col xs={{ span: 12, offset: 1 }} md={colSize}> 
-    <Card
-      size="small"
-      hoverable
-      cover={<img alt={alt} src={src} />}
-    >
-      <Meta title={title} description={description} />
-    </Card>
+  const [statusMouse, setStatusMouse] = useState(false);
+
+  const statusMouseOver = ()=>{
+    setStatusMouse(true)
+  }
+  
+  const statusMouseOut = ()=>{
+    setStatusMouse(false)
+  }
+
+  return(<Col xs={{ span: 12, offset: 1 }} md={colSize} style={{ height: 392,
+    marginBottom: '-78px',
+    whiteSpace: 'nowrap', color: 'white'}}>
+      <Link
+        size="small"
+        hoverable
+        cover={<img alt={alt} src={src} />}
+        onMouseOver={statusMouseOver}
+        onMouseOut={statusMouseOut}
+      >
+       <Meta title={title} description={description} />
+       {statusMouse ? <IconCustom type="heart" theme="twoTone" twoToneColor="#eb2f96" /> : <></>}
+      </Link>
   </Col>)
 }
 
